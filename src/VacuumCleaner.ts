8e98039796e728be7083520d1f890f9f3b63a5f0
DarleiSchmegel <darleischmegel94@gmail.com>
2022-11-12 15:09:22 -0300

    it's working

--- src/VacuumCleaner.ts
+++ src/VacuumCleaner.ts
@@ -1,12 +1,9 @@
 import tests from "./CaseTests.json";
+// import promptSync from 'prompt-sync';
 
 interface VacuumCleanerProps {
   dimensions: { width: number; length: number };
   direction: "North" | "South" | "East" | "West";
-  toSpinValue: 90;
-  toSpinDirection: "left" | "right";
-  movementHistory: string[];
-  place: number[][];
   position: {
     X: number;
     Y: number;
@@ -16,134 +13,167 @@ interface VacuumCleanerProps {
 //coordenadas position sempre <= 0
 //position X sempre < dimensions.width
 //position Y sempre < dimensions.length
-let VacuumCleaner = {
-  direction: "East",
-  dimensions: {
-    length: 0,
-    width: 0,
-  },
-  position: {
-    X: 0,
-    Y: 0,
-  },
-} as VacuumCleanerProps;
 
-tests.data.tests.map(async (test, index: number) => {
-  if (index > 0) return;
+async function executeTests() {
+  tests.data.tests.map(async (test) => {
+    let VacuumCleaner = {
+      direction: "North",
+      dimensions: {
+        width: 0,
+        length: 0,
+      },
+      position: {
+        X: 0,
+        Y: 0,
+      },
+    } as VacuumCleanerProps;
 
-  const dimensions = test.input.line1.split(" ");
-  console.log("s", dimensions, Number(dimensions[0]));
-  VacuumCleaner.dimensions.width = Number(dimensions[0]);
-  VacuumCleaner.dimensions.length = Number(dimensions[1]);
-  let arraySteps = test.input.line2.split("");
+    // if (index > 2) return;
 
-  await arraySteps.forEach(async (step) => {
-    await displacement(step as "F" | "T" | "E" | "D");
+    const dimensions = test.input.line1.split(" ");
+    // console.log("s", dimensions, Number(dimensions[0]));
+    VacuumCleaner.dimensions.width = Number(dimensions[0]);
+    VacuumCleaner.dimensions.length = Number(dimensions[1]);
+    let arraySteps = test.input.line2.split("");
+
+    await arraySteps.forEach(async (step) => {
+      //posição X < width
+      //posição Y < length.
+      // F (indicando um passo para a frente),
+      // T (indicando um passo para trás),
+      // E (indicando uma rotação 90° à esquerda sem se deslocar)
+      // D (indicando uma rotação de 90° para a direita sem se deslocar).
+
+      switch (step) {
+        case "F": //Passo para frente
+          if (
+            VacuumCleaner.direction === "East" &&
+            VacuumCleaner.position.X < VacuumCleaner.dimensions.width
+          ) {
+            VacuumCleaner.position.X += 1;
+            // console.log(VacuumCleaner.position)
+            break;
+          }
+          if (
+            VacuumCleaner.direction === "West" &&
+            VacuumCleaner.position.X > 0
+          ) {
+            VacuumCleaner.position.X -= 1;
+            break;
+          }
+          if (
+            VacuumCleaner.direction === "North" &&
+            VacuumCleaner.position.Y < VacuumCleaner.dimensions.length
+          ) {
+            VacuumCleaner.position.Y += 1;
+            break;
+          }
+          if (
+            VacuumCleaner.direction === "South" &&
+            VacuumCleaner.position.Y > 0
+          ) {
+            VacuumCleaner.position.Y -= 1;
+            break;
+          }
+          break;
+        case "T": //Passo para trás
+          if (
+            VacuumCleaner.direction === "East" &&
+            VacuumCleaner.position.X > 0
+          ) {
+            VacuumCleaner.position.X -= 1;
+            break;
+          }
+          if (
+            VacuumCleaner.direction === "West" &&
+            VacuumCleaner.position.X < VacuumCleaner.dimensions.width
+          ) {
+            VacuumCleaner.position.X += 1;
+            break;
+          }
+          if (
+            VacuumCleaner.direction === "North" &&
+            VacuumCleaner.position.Y > 0
+          ) {
+            VacuumCleaner.position.Y -= 1;
+            break;
+          }
+          if (
+            VacuumCleaner.direction === "South" &&
+            VacuumCleaner.position.Y < VacuumCleaner.dimensions.length
+          ) {
+            VacuumCleaner.position.Y += 1;
+            break;
+          }
+          break;
+        case "E": // Rotação esquerda
+          if (VacuumCleaner.direction === "North") {
+            VacuumCleaner.direction = "West";
+            break;
+          }
+
+          if (VacuumCleaner.direction === "East") {
+            VacuumCleaner.direction = "North";
+            break;
+          }
+          if (VacuumCleaner.direction === "South") {
+            VacuumCleaner.direction = "East";
+            break;
+          }
+          if (VacuumCleaner.direction === "West") {
+            VacuumCleaner.direction = "South";
+            break;
+          }
+          break;
+        case "D": // Rotação deireita
+          if (VacuumCleaner.direction === "North") {
+            VacuumCleaner.direction = "East";
+            break;
+          }
+
+          if (VacuumCleaner.direction === "East") {
+            VacuumCleaner.direction = "South";
+            break;
+          }
+          if (VacuumCleaner.direction === "South") {
+            VacuumCleaner.direction = "West";
+            break;
+          }
+          if (VacuumCleaner.direction === "West") {
+            VacuumCleaner.direction = "North";
+            break;
+          }
+          break;
+
+        default:
+          break;
+      }
+    });
+
     console.log(
-      step,
-      VacuumCleaner.position,
       VacuumCleaner.direction,
-      VacuumCleaner.dimensions
+      VacuumCleaner.position.X,
+      VacuumCleaner.position.Y
     );
   });
-
-  console.log(VacuumCleaner.position, VacuumCleaner.direction);
-});
-
-async function displacement(step: "F" | "T" | "E" | "D") {
-  switch (step) {
-    case "F": //Passo para frente
-      if (VacuumCleaner.direction === "East") {
-        VacuumCleaner.position.Y += 1;
-        return;
-      }
-      if (VacuumCleaner.direction === "West") {
-        VacuumCleaner.position.Y -= 1;
-        return;
-      }
-      if (VacuumCleaner.direction === "North") {
-        VacuumCleaner.position.X -= 1;
-        return;
-      }
-      if (VacuumCleaner.direction === "South") {
-        VacuumCleaner.position.X += 1;
-        return;
-      }
-      break;
-    case "T": //Passo para trás
-      if (VacuumCleaner.direction === "East") {
-        VacuumCleaner.position.Y -= 1;
-        return;
-      }
-      if (VacuumCleaner.direction === "West") {
-        VacuumCleaner.position.Y += 1;
-        return;
-      }
-      if (VacuumCleaner.direction === "North") {
-        VacuumCleaner.position.X += 1;
-        return;
-      }
-      if (VacuumCleaner.direction === "South") {
-        VacuumCleaner.position.X -= 1;
-        return;
-      }
-      break;
-    case "E": // Rotação esquerda
-      if (VacuumCleaner.direction === "North") {
-        VacuumCleaner.direction = "West";
-        return;
-      }
-
-      if (VacuumCleaner.direction === "East") {
-        VacuumCleaner.direction = "North";
-        return;
-      }
-      if (VacuumCleaner.direction === "South") {
-        VacuumCleaner.direction = "East";
-        return;
-      }
-      if (VacuumCleaner.direction === "West") {
-        VacuumCleaner.direction = "South";
-        return;
-      }
-      break;
-    case "D": // Rotação deireita
-      if (VacuumCleaner.direction === "North") {
-        VacuumCleaner.direction = "East";
-        return;
-      }
-
-      if (VacuumCleaner.direction === "East") {
-        VacuumCleaner.direction = "South";
-        return;
-      }
-      if (VacuumCleaner.direction === "South") {
-        VacuumCleaner.direction = "West";
-        return;
-      }
-      if (VacuumCleaner.direction === "West") {
-        VacuumCleaner.direction = "North";
-        return;
-      }
-      break;
-
-    default:
-      break;
-  }
 }
 
-// F (indicando um passo para a frente),
-// T (indicando um passo para trás),
-// E (indicando uma rotação 90° à esquerda sem se deslocar)
-// D (indicando uma rotação de 90° para a direita sem se deslocar).
+async function main() {
+  console.log("=============================");
+  console.log("Testes feitos a partir do arquivo CaseTests.json");
+  await executeTests();
+  console.log("=============================");
 
-// const readline = require("readline").createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-// readline.question(`What's your name?`, (name: any) => {
-//   console.log(`Hi ${name}!`);
-//   readline.close();
-// });
+
+
+// const prompt = promptSync();
+
+// const input = prompt("Digite a entrada: \n");
+
+// console.log(input)
+
+}
+
+main();
 
 // console.log("ola");
